ea9c3dad25e39991bcafb45c8083c045afe3a9f6
DaviGiordano <davigiordano2704@gmail.com>
2025-05-02 18:33:00 +0100

    Move synthetic data generator

--- notebooks/synthetic_data_generator.py
+++ notebooks/synthetic_data_generator.py
@@ -1,13 +1,9 @@
-from dataclasses import dataclass
-from pathlib import Path
 from typing import Dict, List, Literal, Optional, Tuple, Union
+from dataclasses import dataclass
 
-import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import pandas as pd
-import seaborn as sns
-from scipy import stats
 
 
 @dataclass
